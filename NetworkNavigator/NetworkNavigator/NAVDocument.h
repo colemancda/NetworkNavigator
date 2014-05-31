a0639707c5150a9619dc468224d756713c98d67d
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-05-30 23:27:57 -0400

    created document-based project

---
+++ NetworkNavigator/NetworkNavigator/NAVDocument.h
@@ -0,0 +1,13 @@
+//
+//  NAVDocument.h
+//  NetworkNavigator
+//
+//  Created by Alsey Coleman Miller on 5/30/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NAVDocument : NSDocument
+
+@end
---
+++ NetworkNavigator/NetworkNavigator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NetworkNavigator
+//
+//  Created by Alsey Coleman Miller on 5/30/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
