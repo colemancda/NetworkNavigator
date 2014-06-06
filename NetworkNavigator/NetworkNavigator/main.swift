e9c7d8bd2f49aa401315b3ec1e024d47021068e5
Alsey Coleman Miller <alseycmiller@gmail.com>
2014-06-06 11:33:10 -0400

    Made app a Swift project

---
+++ NetworkNavigator/NetworkNavigator/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  NetworkNavigator
+//
+//  Created by Alsey Coleman Miller on 6/6/14.
+//  Copyright (c) 2014 ColemanCDA. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
